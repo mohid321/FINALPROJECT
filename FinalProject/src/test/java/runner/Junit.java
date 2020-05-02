e2fe2cd5cf26f938639d2bac5a756f1c87fc164a
mhoss <mohid321>
2020-05-02 18:35:18 -0400

    final project done

---
+++ FinalProject/src/test/java/runner/Junit.java
@@ -0,0 +1,22 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features = "src/test/resources",
+
+                  glue = { "stepdef" },
+                  
+                  tags = { " @test001" },
+                  
+                  plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/report/report.html" },
+  
+                  monochrome = true
+                		  )
+public class Junit {
+
+}
